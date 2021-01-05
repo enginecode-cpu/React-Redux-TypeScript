e38bbd2d0f5984378ed6c8e672a7f11fd45779a2
enginecode-cpu <bassmatics1002@gmail.com>
2021-01-05 22:23:28 +0900

    파일 분리

---
+++ src/modules/todos/reducer.ts
@@ -0,0 +1,21 @@
+import { createReducer } from "typesafe-actions";
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from "./actions";
+import { TodosAction, TodoState } from "./types";
+
+const initialState: TodoState = [];
+
+const todos = createReducer<TodoState, TodosAction>(initialState, {
+  [ADD_TODO]: (state, action) =>
+    state.concat({
+      ...action.payload,
+      done: false,
+    }),
+  [TOGGLE_TODO]: (state, action) =>
+    state.map((todo) =>
+      todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+    ),
+  [REMOVE_TODO]: (state, action) =>
+    state.filter((todo) => todo.id !== action.payload),
+});
+
+export default todos;
